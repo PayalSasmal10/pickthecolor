5defa04689dd4175c36d7e78381d69f7cfe8406e
Payal Sasmal <sasmalpayal@gmail.com>
2023-07-17 20:28:35 +0530

    using sketch picker

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-import { ColorPicker, useColor } from 'react-color-palette';
-import "react-color-palette/lib/css/styles.css";
+
 import { SketchPicker } from 'react-color';
+import { useState } from 'react';
 
 function App() {
-  const [color, setColor] = useColor("lightblue");
+  const [color, setColor] = useState("lightblue");
   console.log(color);
   console.log(setColor);
   return (
-    <div>
+    <div className='App'>
       <h1>Color Picker</h1>
       <SketchPicker
         color={color}
-        onChange={setColor}
+        onChange={(updateColor) => setColor(updateColor.hex)}
        />
     </div>
   );
